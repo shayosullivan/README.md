f2d056e3b1ac946ce6e68a80dbc76b0bc51f8510
shay <shayosullivan@hotmail.com>
2022-06-03 15:16:52 -0400

    Licenses

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,48 @@
 // TODO: Include packages needed for this application
 
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+const generateMarkdown = ({
+  title,
+  description,
+  install,
+  usage,
+  contribute,
+  test,
+  license,
+  github,
+  linkedIn,
+}) => {
+  return `# ${title}
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+    
+## Description
+${description}
+## Installation
+${install}
+## Usage
+${usage}
+## License
+${license}
+## Contributing
+${contribute}
+## Tests
+${test}
+## Questions
+#### GitHub Profile
+[${github}](https://github.com/shayosullivan) 
+#### LinkedIn Profile
+[${linkedIn}](https://www.linkedin.com/in/seamus-o-sullivan-2274b1238/) 
+  `;
+};
+
 // TODO: Create an array of questions for user input
 const questions = [];
 

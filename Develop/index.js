daa64c0f97bc71f4aac1396d2e1aff33355bcb44
shay <shayosullivan@hotmail.com>
2022-06-03 15:18:09 -0400

    Questions

--- Develop/index.js
+++ Develop/index.js
@@ -44,13 +44,64 @@ ${test}
 };
 
 // TODO: Create an array of questions for user input
-const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// TODO: Create an array of questions for user input
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "Project title:",
+      name: "title",
+    },
+    {
+      type: "input",
+      message: "Description of project:",
+      name: "description",
+    },
+    {
+      type: "input",
+      message: "How to Install:",
+      name: "install",
+    },
+    {
+      type: "input",
+      message: "Usage Info:",
+      name: "usage",
+    },
+    {
+      type: "input",
+      message: "How do you Contribute:",
+      name: "contribute",
+    },
 
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+    {
+      type: "list",
+      message: "What license did you use:",
+      name: "license",
+      choices: [
+        "The MIT License",
+        "The GPL License",
+        "Apache License",
+        "GNU License",
+      ],
+    },
+    {
+      type: "input",
+      message: "GitHub username:",
+      name: "github",
+    },
+    {
+      type: "input",
+      message: "LinkedIn username:",
+      name: "linkedIn",
+    },
+  ])
+  .then((data) => {
+    //generateMarkdown(data);
+    fs.writeFile("README.md", generateMarkdown(data), (err) => {
+      if (err) console.log(err);
+      else {
+        console.log("Success!");
+      }
+    });
+  });
